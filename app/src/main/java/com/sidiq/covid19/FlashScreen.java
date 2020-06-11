884a852e214cff18868f34a1527e4e1e27676ab4
hidayatul sidiq <omegaleonidas33@gmail.com>
2020-06-11 21:43:14 +0700

    update baru

--- app/src/main/java/com/sidiq/covid19/FlashScreen.java
+++ app/src/main/java/com/sidiq/covid19/FlashScreen.java
@@ -2,6 +2,7 @@ package com.sidiq.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class FlashScreen extends AppCompatActivity {
@@ -10,5 +11,22 @@ public class FlashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_screen);
+
+        final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
